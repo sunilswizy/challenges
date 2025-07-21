857d8d954b541aa3b71ea948fa7edf3a33a6b629
sunil_swizy <sunil@iamneo.ai>
2025-07-21 23:49:23 +0530

    Implement jsonToMatrix function and initialize array of objects

---
+++ array-of-objects-to-json.js
@@ -0,0 +1,15 @@
+
+const arr = [
+    { "b": 1, "a": 2 },
+    { "b": 2, "a": 1 },
+];
+
+
+function jsonToMatrix(arr) {
+    const keys = Object.keys(arr).sort();
+
+    for(let key of keys) {
+        
+    }
+
+}
