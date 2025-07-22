c1e2d20d9a7b52436ba0cdfb702e661fe7f93e2e
sunil_swizy <sunil@iamneo.ai>
2025-07-23 00:02:28 +0530

    Array of objects to JSON - Implementation

--- array-of-objects-to-json.js
+++ array-of-objects-to-json.js
@@ -4,12 +4,61 @@ const arr = [
     { "b": 2, "a": 1 },
 ];
 
+console.log(jsonToMatrix(arr));
+
 
 function jsonToMatrix(arr) {
-    const keys = Object.keys(arr).sort();
+    
+    const keySet = new Set()
 
-    for(let key of keys) {
-        
+    for(const obj of arr) {
+        getKeys(obj, "")
     }
 
+    const sortedKeys = Array.from(keySet).sort();
+    const res = [];
+    res.push(sortedKeys)
+
+    for(const obj of arr) {
+        const keyToVal = {};
+        getValues(obj, "", keyToVal);
+        const row = [];
+
+        for(const key of sortedKeys) {
+            row.push(key in keyToVal ? keyToVal[key] : "")
+        }
+        res.push(row);
+    }
+
+    return res
+
+
+    function getKeys(obj, path) {
+        for(const key in obj) {
+            const newPath = path ? `${path}.${key}`: key;
+            if(isObject(obj[key])) {
+                getKeys(obj[key], newPath)
+            }
+            else {
+                keySet.add(key);
+            }
+        }
+    }
+
+    function isObject(obj) {
+        return obj !== null && typeof obj === 'object'
+    }
+
+    function getValues(obj, path, keyToVal) {
+        for(const key in obj) {
+            const newPath = path ? `${path}.${key}`: key;
+            if(isObject(obj[key])) {
+                getValues(obj[key], newPath, keyToVal)
+            }
+            else {
+                keyToVal[newPath] = obj[key]
+            }
+        }
+    }
+    
 }
